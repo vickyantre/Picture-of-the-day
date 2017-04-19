22d1465f469a305dedf226a9b32696cf701de7f2
vickyantre <vicky.antre@gmail.com>
2017-04-19 17:15:34 +0300

    made some changes in today lists, mane calendar classes and footer, created registration form

--- public/js/list.js
+++ public/js/list.js
@@ -1,19 +1,9 @@
 var inputText = document.querySelector("#todoText");
-var todosList = document.querySelector("#todoList");
-var todosLeft = document.querySelector("#todosLeft");
-var clearCompleted = document.querySelector("#clearCompleted");
-var markAllComplited = document.querySelector("#markAllComplited")
 var todoIndexValue = 0;
+var todosList = document.querySelector("#todoList");
 
-var showAll = document.querySelector("#showAll");
-var showActive = document.querySelector("#showActive");
-var showCompleted = document.querySelector("#showCompleted");
+todos = [];
 
-var globalTodoFilter = null;
-
-var todos = [];
-
-// inputText.onkeypress = function(e) {
 $("#todoText").keypress(function(e){
     if (e.keyCode == 13) {
         todoIndexValue++;
@@ -22,100 +12,30 @@ $("#todoText").keypress(function(e){
             isDone: false,
             index: todoIndexValue
         });
-        updateLocalStorage();
         inputText.value = "";
         renderTodos();
-        countActiveTodos();
+        updateLocalStorage();
     }
 });
 
+function renderTodos(){
 
-
-function changeTodoStatus(todo,liClass, todoState){
-            var li = document.querySelector("li[todo-index='" + todo.index + "']");
-            var checkbox = li.querySelector("input");
-
-            todo.isDone = todoState;
-            checkbox.checked = todoState;
-            li.setAttribute("class", "list-group-item " + liClass);
-            updateLocalStorage();
-}
-
-
-
-function renderTodos(todoFilter) {
-    globalTodoFilter = todoFilter;
-
-    var filteredTodos = todos;
-    todosList.innerHTML = "";
-
-    if (todos.length == 0) {
         todosList.innerHTML = "";
-        return;
-    }
 
-    if (todoFilter != null) {
-        todosList.innerHTML = "";
-        filteredTodos = filteredTodos.filter(function(todo) {
-            return todo.isDone == todoFilter;
-        });
-    }
-
-    filteredTodos.forEach(function(todo) {
+        todos.forEach(function(todo){            
         var todoElementTemplate = document.querySelector("div#hollow li").cloneNode(true);
-
         todoElementTemplate.querySelector("span").innerText = todo.text;
-        todoElementTemplate.setAttribute("todo-index", todo.index)
-        
-        if(todo.isDone == true){
-            todoElementTemplate.setAttribute("class", "list-group-item todo-done")
-            todoElementTemplate.querySelector("input").checked= true;
-        }
+        todosList.appendChild(todoElementTemplate);
 
-        todoElementTemplate.querySelector("input").onchange = function(e) {
-            var li = e.path[1];
-            var todoIndex = li.getAttribute("todo-index");
-            var todo = todos.filter(function(todo) {
-                return todo.index == todoIndex;
-            });
-
-            todo = todos.indexOf(todo[0]);
-            todo = todos[todo];
-
-            if(e.path[0].checked) {
-                li.setAttribute("class"," list-group-item todo-done");
-                todo.isDone = true;
-            } else {
-                li.setAttribute("class","list-group-item");
-                todo.isDone = false;
-            }
-            countActiveTodos();
-            showClearCompleted();
-            showMarkAllButton();
-            updateLocalStorage();
-        }
         todoElementTemplate.querySelector("button").onclick = function(e) {
             var li = e.path[1];
-            var todoIndex = li.getAttribute("todo-index");
-            todos.splice(todoIndex, 1);
-
             todosList.removeChild(li);
-            // renderTodos();
-            countActiveTodos();
-            showClearCompleted();
-            showMarkAllButton()
-            updateLocalStorage();
         }
-        todosList.appendChild(todoElementTemplate);
     });
+
+        updateLocalStorage();
 }
 
-
-function countActiveTodos() {
-    var activeTodos = todos.filter(function(todo){
-        return todo.isDone == false;
-    });
-}
 
 function updateLocalStorage(){
     localStorage.setItem("todos", JSON.stringify(todos));
@@ -127,9 +47,87 @@ function init(){
     if (localStorageTodos != undefined){
       todos = JSON.parse(localStorageTodos);  
     }
-    
-    renderTodos(null);
-    countActiveTodos(); 
+    renderTodos(); 
 }
 
 init();
+
+
+
+
+
+
+
+
+
+
+var inputTextP = document.querySelector("#todoTextPlan");
+var todoIndexValueP = 0;
+var todosListP = document.querySelector("#todoListPlan");
+
+todosP = [];
+
+$("#todoTextPlan").keypress(function(e){
+    if (e.keyCode == 13) {
+        todoIndexValueP++;
+        todosP.push({
+            text: inputTextP.value,
+            isDone: false,
+            index: todoIndexValueP
+        });
+        inputTextP.value = "";
+        renderTodosP();
+        updateLocalStorageP();
+    }
+});
+
+function renderTodosP(){    
+        todosListP.innerHTML = "";
+
+        todosP.forEach(function(todo){            
+         var todoElementTemplateP = document.querySelector("div#hollowPlan li").cloneNode(true);
+
+
+        todoElementTemplateP.querySelector("input").onchange = function(e){
+            var li = e.path[1];
+
+            if(e.path[0].checked) {
+                li.classList.add("todo-done");
+                $(li).parent().append($(li));
+                todo.isDone = true;
+            } else {
+                li.classList.remove("todo-done");
+                $(li).parent().prepend($(li));
+                todo.isDone = false;
+            }
+            updateLocalStorageP();
+        };
+
+        todoElementTemplateP.querySelector("span").innerText = todo.text;
+        todosListP.appendChild(todoElementTemplateP);
+
+        todoElementTemplateP.querySelector("button").onclick = function(e) {
+            var li = e.path[1];
+            todosListP.removeChild(li);
+        }
+
+    });
+
+        updateLocalStorageP();
+}
+
+
+function updateLocalStorageP(){
+    localStorage.setItem("todosP", JSON.stringify(todosP));
+}
+
+
+function initP(){
+    var localStorageTodosP = localStorage.todosP;
+    if (localStorageTodosP != undefined){
+      todosP = JSON.parse(localStorageTodosP);  
+    }
+    renderTodosP(); 
+}
+
+initP();
